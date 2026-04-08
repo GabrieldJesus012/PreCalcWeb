4033c2e075ec49e512577b36cfa8c53532c7045a
Gabriel de Jesus Silva <154984056+GabrieldJesus012@users.noreply.github.com>
2026-04-08 19:19:38 -0300

    adicionando total bruto + valor total atualizado formatado

--- precalc/core/admin.py
+++ precalc/core/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db.models import Sum
 from core.models import Calculo, CalculoCredor
 
 
@@ -11,12 +12,23 @@ class CalculoCredorInline(admin.TabularInline):
 
 @admin.register(Calculo)
 class CalculoAdmin(admin.ModelAdmin):
-    list_display = ['numero_processo', 'beneficiario', 'natureza', 'tipo_calculo', 'valor_total', 'data_calculo']
+    list_display = ['numero_processo', 'beneficiario', 'natureza', 'tipo_calculo', 'valor_total_fmt', 'total_pago', 'data_calculo']
     list_filter = ['natureza', 'tipo_calculo', 'tem_herdeiros', 'tem_honorario_sucumbencial', 'data_calculo']
     search_fields = ['numero_processo', 'beneficiario', 'credor']
     readonly_fields = ['data_calculo', 'dados_entrada', 'resultado_completo']
     inlines = [CalculoCredorInline]
 
+    
+    def valor_total_fmt(self, obj):
+        if obj.valor_total is None:
+            return '-'
+        return f'R$ {obj.valor_total:,.2f}'.replace(',', 'X').replace('.', ',').replace('X', '.')
+    valor_total_fmt.short_description = 'Total Atualizado'
+
+    def total_pago(self, obj):
+        total = obj.credores.aggregate(Sum('valor_bruto'))['valor_bruto__sum'] or 0
+        return f'R$ {total:,.2f}'.replace(',', 'X').replace('.', ',').replace('X', '.')
+    total_pago.short_description = 'Total Pago (Bruto)'
 
 @admin.register(CalculoCredor)
 class CalculoCredorAdmin(admin.ModelAdmin):
